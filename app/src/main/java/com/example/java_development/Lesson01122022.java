23fce701588f240999d8a17dbbf8a0610e909b99
Samsung1 <mihail.glazov2015@yandex.ru>
2022-12-01 17:59:15 +0300

    01.12.2022

---
+++ app/src/main/java/com/example/java_development/Lesson01122022.java
@@ -0,0 +1,68 @@
+package com.example.java_development;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.java_development.databinding.ActivityLesson01122022Binding;
+
+public class Lesson01122022 extends AppCompatActivity {
+    ActivityLesson01122022Binding binding;
+    EditText eT1;
+    EditText eT2;
+    Button button;
+
+    @SuppressLint("WrongViewCast")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lesson01122022);
+        this.binding = ActivityLesson01122022Binding.inflate(getLayoutInflater());
+        eT1 = findViewById(R.id.eT1);
+        eT2 = findViewById(R.id.eT2);
+        button = findViewById(R.id.button);
+        eT1.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                if (!(eT1.getText().toString().equals(eT2.getText().toString()))) {
+                    eT2.setText(editable);
+                }
+            }
+        });
+        eT2.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                if (!(eT1.getText().toString().equals(eT2.getText().toString()))) {
+                    eT1.setText(editable);
+                }
+            }
+        });
+        button.setOnClickListener(view -> {
+            eT1.setText("");
+        });
+    }
+}
--- app/src/main/java/com/example/java_development/LessonActivity.java
+++ app/src/main/java/com/example/java_development/LessonActivity.java
@@ -1,62 +1,50 @@
 package com.example.java_development;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
 
-import java.util.Random;
-import java.util.concurrent.ThreadLocalRandom;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.java_development.databinding.ActivityLessonBinding;
 
 public class LessonActivity extends AppCompatActivity {
-    Button button2;
-    Button button3;
-    Button button4;
-    TextView textView2;
-    EditText editTextTextPassword;
+    ActivityLessonBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lesson);
-        button2 = findViewById(R.id.button2);
-        button3 = findViewById(R.id.button3);
-        button4 = findViewById(R.id.button4);
-        textView2 = findViewById(R.id.textView2);
-        editTextTextPassword = findViewById(R.id.editTextTextPassword);
+        binding = ActivityLessonBinding.inflate(getLayoutInflater());
         View.OnClickListener listener = this::action;
-        button2.setOnClickListener(listener);
-        button3.setOnClickListener(listener);
-        button4.setOnClickListener(listener);
-        textView2.setOnClickListener(listener);
-        editTextTextPassword.setOnClickListener(listener);
+        binding.button2.setOnClickListener(listener);
+        binding.button3.setOnClickListener(listener);
+        binding.button4.setOnClickListener(listener);
+        binding.textView2.setOnClickListener(listener);
+        binding.editTextTextPassword.setOnClickListener(listener);
     }
 
     public void action(View v) {
         switch (v.getId()) {
             case R.id.button2:
                 Toast.makeText(this, "Кнопка 2", Toast.LENGTH_SHORT).show();
-                String x = button2.getText().toString();
-                button2.setText(new StringBuilder(x).reverse().toString());
+                String x = binding.button2.getText().toString();
+                binding.button2.setText(new StringBuilder(x).reverse().toString());
                 break;
             case R.id.button3:
                 x = new StringBuilder(((int) (Math.random() * 5)) + "").toString();
-                button3.setText(x);
+                binding.button3.setText(x);
                 System.out.println(x);
-                button3.setBackgroundColor(getResources().getColor(R.color.purple_200));
+                binding.button3.setBackgroundColor(getResources().getColor(R.color.purple_200));
                 break;
             case R.id.button4:
-                button4.setTextColor(getResources().getColor(R.color.black));
+                binding.button4.setTextColor(getResources().getColor(R.color.black));
                 break;
             case R.id.textView2:
-                textView2.setVisibility(View.INVISIBLE);
+                binding.textView2.setVisibility(View.INVISIBLE);
                 break;
             case R.id.editTextTextPassword:
-                x = editTextTextPassword.getText().toString();
-                editTextTextPassword.setText(new StringBuilder(x).append(1).reverse().toString());
+                x = binding.editTextTextPassword.getText().toString();
+                binding.editTextTextPassword.setText(new StringBuilder(x).append(1).reverse().toString());
                 break;
             default:
                 System.out.println("...");
