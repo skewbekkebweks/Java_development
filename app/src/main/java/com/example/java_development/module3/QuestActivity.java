63783341d7b7617cf7f37f35e124b321c7f59993
Samsung1 <mihail.glazov2015@yandex.ru>
2022-12-30 16:05:22 +0300

    30.12.2022

--- app/src/main/java/com/example/java_development/module3/QuestActivity.java
+++ app/src/main/java/com/example/java_development/module3/QuestActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.example.java_development.databinding.ActivityQuestBinding;
 
@@ -18,17 +19,31 @@ public class QuestActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         this.binding = ActivityQuestBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+        character = new Character(binding.editTextTextPersonName.getText().toString());
         story = new Story();
+
         show();
 
 
     }
 
     private void show() {
-
+        binding.career.setText("K = " + character.K);
+        binding.assets.setText("A = " + character.A + "");
+        binding.reputation.setText("R = " + character.R + "");
         binding.text.setText(story.current_situation.text);
         binding.subject.setText(story.current_situation.subject);
         int count = story.current_situation.direction.length;
+        binding.buttons.removeAllViews();
+        binding.restart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                character = new Character(binding.editTextTextPersonName.getText().toString());
+                story = new Story();
+                show();
+
+            }
+        });
         if (count > 0) {
             for (int i = 0; i < count; i++) {
                 Button b = new Button(getBaseContext());
@@ -40,10 +55,18 @@ public class QuestActivity extends AppCompatActivity {
                     @Override
                     public void onClick(View view) {
                         story.go(a);
+                        character.K += story.current_situation.dK;
+                        character.A += story.current_situation.dA;
+                        character.R += story.current_situation.dR;
                         show();
                     }
                 });
             }
+        } else {
+            character.name = binding.editTextTextPersonName.getText().toString();
+            TextView textView = new TextView(getBaseContext());
+            textView.setText(character.name + " прошёл квест");
+            binding.buttons.addView(textView);
         }
     }
 }
--- app/src/main/java/com/example/java_development/module3/Situation.java
+++ app/src/main/java/com/example/java_development/module3/Situation.java
@@ -2,14 +2,16 @@ package com.example.java_development.module3;
 
 public class Situation {
     Situation[] direction;
+    int[] directionIndexes;
     String subject,text;
     int dK,dA,dR;
-    public Situation (String subject, String text, int variants, int dk,int da,int dr) {
+    public Situation (String subject, String text, int[] variantsIndexes, int dk,int da,int dr) {
         this.subject=subject;
         this.text=text;
         dK=dk;
         dA=da;
         dR=dr;
-        direction=new Situation[variants];
+        directionIndexes = variantsIndexes;
+        direction=new Situation[variantsIndexes.length];
     }
 }
